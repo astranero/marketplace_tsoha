3b55a478fe647b5a685f8058b0fd2d83d05381a6
Neroniuoso <esa.taheresa@gmail.com>
2022-08-22 18:54:17 +0300

    Nothing

--- schema.py
+++ schema.py
@@ -1,11 +1,10 @@
 import os
 import psycopg2
-
 conn = psycopg2.connect(
     host=os.getenv("DB_HOST"),
     database=os.getenv("DB_NAME"),
     user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
+    password=os.getenv("DB_PASSWORD")
 )
 
 cur = conn.cursor()
