c7e51a3e2e1ccf2e2cbaa40c2ed3ea1329b9ae86
astranero <esa.taheresa@gmail.com>
2022-09-01 19:13:44 +0300

    Fixed a typo

--- src/__init__.py
+++ src/__init__.py
@@ -8,7 +8,7 @@ load_dotenv("/home/koxhr/0000 Repositories/marketplace_tsoha/.env")
 app = Flask(__name__)
 app.config["SECRET_KEY"] = environ.get("SECRET_KEY")
 app.config["UPLOAD_FOLDER"] = environ.get("UPLOAD_FOLDER")
-app.config["SQLALCHEMY_DATABASE_URI"] = environ.get("DATABASE_URL").replace("postgress://", "postgresql://")
+app.config["SQLALCHEMY_DATABASE_URI"] = environ.get("DATABASE_URL").replace("postgres://", "postgresql://")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config[" TEMPLATES_AUTO_RELOAD"] = True
 app.permanent_session_lifetime = timedelta(hours=24)
