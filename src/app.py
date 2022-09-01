7d1d4c4c7ac6dd37a4caf96f825a1f5be171a264
astranero <esa.taheresa@gmail.com>
2022-09-01 18:49:46 +0300

    Fixed altered files

--- src/app.py
+++ src/app.py
@@ -1,17 +1,4 @@
 from src.views import app, views
-from os import environ
-from datetime import timedelta
-from flask_login import LoginManager
-app.config["SECRET_KEY"] = environ.get("SECRET_KEY")
-app.config["UPLOAD_FOLDER"] = environ.get("UPLOAD_FOLDER")
-app.config["SQLALCHEMY_DATABASE_URI"] = environ.get("DATABASE_URL")
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config[" TEMPLATES_AUTO_RELOAD"] = True
-app.permanent_session_lifetime = timedelta(hours=24)
-login_manager = LoginManager()
-login_manager.login_view = 'views.login'
-login_manager.session_protection = "strong"
-login_manager.login_message_category = 'info'
 app.register_blueprint(views, url_prefix="/")
 
 if __name__ == "__main__":
