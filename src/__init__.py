a672621ff29411ab46840e5c6a6a36dc790d99d8
astranero <esa.taheresa@gmail.com>
2022-09-01 18:56:37 +0300

    Added path to postgresql

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
-from os import environ
+from os import getenv, environ
 from datetime import timedelta
 from flask_login import LoginManager
 from flask import Flask
@@ -8,7 +8,7 @@ load_dotenv("/home/koxhr/0000 Repositories/marketplace_tsoha/.env")
 app = Flask(__name__)
 app.config["SECRET_KEY"] = environ.get("SECRET_KEY")
 app.config["UPLOAD_FOLDER"] = environ.get("UPLOAD_FOLDER")
-app.config["SQLALCHEMY_DATABASE_URI"] = environ.get("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] =  getenv("DATABASE_URL").replace("postgres:///", "postgresql:///", 1)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config[" TEMPLATES_AUTO_RELOAD"] = True
 app.permanent_session_lifetime = timedelta(hours=24)
